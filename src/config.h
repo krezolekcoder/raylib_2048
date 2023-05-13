49115b8d7bd1537104c812f3845a4f532af163b7
krezolekcoder <kkrezole@gmail.com>
2023-05-13 14:27:03 +0200

    minor refactor

--- src/config.h
+++ src/config.h
@@ -4,15 +4,15 @@
 
 #define MAX_KEY_MAPPINGS (3U)
 #define KEYS_CNT         (4U)
+#define TILE_CNT_ROW     (4U)
 
-#define BLOCK_SIZE              (125U)
-#define BLOCK_OFFSET            (18U)
-#define SCREEN_WIDTH_BLOCK_CNT  (4U)
-#define SCREEN_HEIGHT_BLOCK_CNT (4U)
-#define SCREEN_WIDTH            (BLOCK_SIZE * SCREEN_WIDTH_BLOCK_CNT)
-#define SCREEN_HEIGHT           (BLOCK_SIZE * SCREEN_HEIGHT_BLOCK_CNT)
-#define FONT_SIZE               (32U)
-#define TEXT_IN_BLOCK_OFFSET    (5U)
+
+#define BLOCK_SIZE           (125U)
+#define BLOCK_OFFSET         (18U)
+#define SCREEN_WIDTH         (BLOCK_SIZE * TILE_CNT_ROW)
+#define SCREEN_HEIGHT        (BLOCK_SIZE * TILE_CNT_ROW)
+#define FONT_SIZE            (32U)
+#define TEXT_IN_BLOCK_OFFSET (5U)
 
 
 
--- src/core_2048.c
+++ src/core_2048.c
@@ -3,7 +3,7 @@
 #include "config.h"
 #include "platform_port.h"
 
-#define TILE_CNT_ROW (4U)
+
 static const keyboard_mapping_t prv_keys_mapping[KEY_PRESSED_TYPES_CNT] = {
     [KEY_TYPE_UP].key_mappings    = {KEY_W,  KEY_UP,    KEY_KP_8},
     [KEY_TYPE_DOWN].key_mappings  = { KEY_S, KEY_DOWN,  KEY_KP_2},
@@ -11,22 +11,20 @@ static const keyboard_mapping_t prv_keys_mapping[KEY_PRESSED_TYPES_CNT] = {
     [KEY_TYPE_RIGHT].key_mappings = { KEY_D, KEY_RIGHT, KEY_KP_6},
 };
 
-static game_tile_t prv_game_tiles[SCREEN_WIDTH_BLOCK_CNT][SCREEN_HEIGHT_BLOCK_CNT];
+static game_tile_t prv_game_tiles[TILE_CNT_ROW][TILE_CNT_ROW];
 
 static void prv_key_pressed_callback(key_pressed_types_t key_pressed_type);
-
-
 
 void core_2048_tiles_init(uint32_t first_tile_init_pos, uint32_t second_tile_init_pos)
 {
 
-    for (int x_tile_pos = 0; x_tile_pos < SCREEN_WIDTH_BLOCK_CNT; x_tile_pos++) {
-        for (int y_tile_pos = 0; y_tile_pos < SCREEN_HEIGHT_BLOCK_CNT; y_tile_pos++) {
+    for (int x_tile_pos = 0; x_tile_pos < TILE_CNT_ROW; x_tile_pos++) {
+        for (int y_tile_pos = 0; y_tile_pos < TILE_CNT_ROW; y_tile_pos++) {
 
 
             prv_game_tiles[x_tile_pos][y_tile_pos].font_color = BLACK;
 
-            if (x_tile_pos * SCREEN_WIDTH_BLOCK_CNT + y_tile_pos == first_tile_init_pos) {
+            if (x_tile_pos * TILE_CNT_ROW + y_tile_pos == first_tile_init_pos) {
                 prv_game_tiles[x_tile_pos][y_tile_pos].tile_color = COLOR_2;
                 prv_game_tiles[x_tile_pos][y_tile_pos].score      = 2;
             }
@@ -53,7 +51,7 @@ void core_2048_check_key_pressed(KeyboardKey key_pressed)
 
 game_tile_t *core_2048_get_tile(uint32_t x_coord, uint32_t y_coord)
 {
-    if (x_coord >= SCREEN_WIDTH_BLOCK_CNT || y_coord >= SCREEN_HEIGHT_BLOCK_CNT) {
+    if (x_coord >= TILE_CNT_ROW || y_coord >= TILE_CNT_ROW) {
         return NULL;
     }
 
@@ -65,12 +63,7 @@ static void prv_key_pressed_callback(key_pressed_types_t key_pressed_type)
 {
     switch (key_pressed_type) {
     case KEY_TYPE_UP:
-        // printf("KEY TYPE UP ! ");
 
-        for (int x_tile_pos = 0; x_tile_pos < SCREEN_WIDTH_BLOCK_CNT; x_tile_pos++) {
-            for (int y_tile_pos = 0; y_tile_pos < SCREEN_HEIGHT_BLOCK_CNT; y_tile_pos++) {
-            }
-        }
         break;
 
     case KEY_TYPE_DOWN:
@@ -80,39 +73,28 @@ static void prv_key_pressed_callback(key_pressed_types_t key_pressed_type)
     case KEY_TYPE_LEFT:
         printf("KEY TYPE LEFT ! ");
 
-        printf("\r\n");
 
-        printf("SCORE : ");
-        for (int x_tile_pos = 0; x_tile_pos < TILE_CNT_ROW; x_tile_pos++) {
-            printf("%d ", prv_game_tiles[0][x_tile_pos].score);
-        }
+        for (int y_tile_pos = 0; y_tile_pos < TILE_CNT_ROW; y_tile_pos++) {
+            for (int x_tile_pos = 1; x_tile_pos < TILE_CNT_ROW; x_tile_pos++) {
 
-        for (int x_tile_pos = 1; x_tile_pos < TILE_CNT_ROW; x_tile_pos++) {
+                for (int x_possible_movement = x_tile_pos - 1; x_possible_movement >= 0; x_possible_movement--) {
+                    printf("X %d POSSIBLE MOVEMENT : %d \r\n", x_tile_pos, x_possible_movement);
 
-            for (int x_possible_movement = x_tile_pos - 1; x_possible_movement >= 0; x_possible_movement--) {
-                printf("X %d POSSIBLE MOVEMENT : %d \r\n", x_tile_pos, x_possible_movement);
-
-                if (prv_game_tiles[0][x_possible_movement].score == 0) {
-                    prv_game_tiles[0][x_possible_movement].score =
-                        prv_game_tiles[0][x_possible_movement + 1].score;
-                    prv_game_tiles[0][x_possible_movement + 1].score = 0;
+                    if (prv_game_tiles[y_tile_pos][x_possible_movement].score == 0) {
+                        prv_game_tiles[y_tile_pos][x_possible_movement].score =
+                            prv_game_tiles[y_tile_pos][x_possible_movement + 1].score;
+                        prv_game_tiles[y_tile_pos][x_possible_movement + 1].score = 0;
+                    }
+                    else if (prv_game_tiles[y_tile_pos][x_possible_movement + 1].score ==
+                             prv_game_tiles[y_tile_pos][x_possible_movement].score) {
+                        prv_game_tiles[y_tile_pos][x_possible_movement].score +=
+                            prv_game_tiles[y_tile_pos][x_possible_movement + 1].score;
+                        prv_game_tiles[y_tile_pos][x_possible_movement + 1].score = 0;
+                    }
                 }
-                else if (prv_game_tiles[0][x_possible_movement + 1].score ==
-                         prv_game_tiles[0][x_possible_movement].score) {
-                    prv_game_tiles[0][x_possible_movement].score +=
-                        prv_game_tiles[0][x_possible_movement + 1].score;
-                    prv_game_tiles[0][x_possible_movement + 1].score = 0;
-                }
-                // else {
-                //     ;
-                // }
             }
         }
 
-        printf("SCORE : ");
-        for (int x_tile_pos = 0; x_tile_pos < TILE_CNT_ROW; x_tile_pos++) {
-            printf("%d ", prv_game_tiles[0][x_tile_pos].score);
-        }
 
         break;
 
@@ -128,8 +110,8 @@ static void prv_key_pressed_callback(key_pressed_types_t key_pressed_type)
 
 void core_2048_draw_grid(void)
 {
-    for (int i = 0; i < SCREEN_WIDTH_BLOCK_CNT; i++) {
-        for (int j = 0; j < SCREEN_HEIGHT_BLOCK_CNT; j++) {
+    for (int i = 0; i < TILE_CNT_ROW; i++) {
+        for (int j = 0; j < TILE_CNT_ROW; j++) {
             platform_port_draw_tile(prv_game_tiles[i][j].x_pos, prv_game_tiles[i][j].y_pos,
                                     prv_game_tiles[i][j].tile_color, prv_game_tiles[i][j].score,
                                     prv_game_tiles[i][j].font_color);
